62edba533a4ffa8b7d62ef431e3fb680bc1cf727
gui1535 <guilherme.araujo1535@gmail.com>
2023-05-29 00:38:59 -0300

    Component FeaturedRow

---
+++ components/FeaturedRow.js
@@ -0,0 +1,70 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCard from './RestaurantCard'
+
+const FeaturedRow = ({ id, title, description }) => {
+    return (
+        <View>
+            <View className="mt-4 flex-row items-center justify-between px-4">
+                <Text className="font-bold text-lg">{title}</Text>
+                <ArrowRightIcon color="#00CCBB" />
+            </View>
+
+            <Text className="text-xs text-gray-500 px-4">
+                {description}
+            </Text>
+
+            <ScrollView
+                horizontal
+                contentContainerStyle={{
+                    paddingHorizontal: 15,
+                }}
+                showsHorizontalScrollIndicator={false}
+                className="pt-4"
+            >
+                {/* RestaurantCards... */}
+                <RestaurantCard
+                    id={123}
+                    imgUrl="https://links.papareact.com/gn7"
+                    title="Yo! Sushi"
+                    rating={4.5}
+                    genre="Japonês"
+                    address="123 rua teste"
+                    short_description="Descrição do local"
+                    dishes={[]}
+                    long={20}
+                    lat={0}
+                />
+
+                <RestaurantCard
+                    id={123}
+                    imgUrl="https://links.papareact.com/gn7"
+                    title="Yo! Sushi"
+                    rating={4.5}
+                    genre="Japonês"
+                    address="123 rua teste"
+                    short_description="Descrição do local"
+                    dishes={[]}
+                    long={20}
+                    lat={0}
+                />
+
+                <RestaurantCard
+                    id={123}
+                    imgUrl="https://links.papareact.com/gn7"
+                    title="Yo! Sushi"
+                    rating={4.5}
+                    genre="Japonês"
+                    address="123 rua teste"
+                    short_description="Descrição do local"
+                    dishes={[]}
+                    long={20}
+                    lat={0}
+                />
+            </ScrollView>
+        </View>
+    )
+}
+
+export default FeaturedRow
