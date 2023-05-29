16486676894ed6cb358e5e3e2beb5e55b8985351
gui1535 <guilherme.araujo1535@gmail.com>
2023-05-29 00:38:27 -0300

    Estruturando pastas e dependencias

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import HomeScreen from './screens/HomeScreen';
 
+const Stack = createNativeStackNavigator();
+
+
+TailwindProvider
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen}/>
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
+
